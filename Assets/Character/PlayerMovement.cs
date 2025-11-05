83ebe38ed1736d7769f81801006d364ac84d7b3f
MatteoKaz <matteokazan@gmail.com>
2025-11-05 10:51:44 +0100

    Personnage

---
+++ Assets/Character/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed = -0.5f;
+    private Vector3 Direction;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb != null)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x , rb.linearVelocity.y, speed) ;
+        }
+
+
+
+    }
+}
