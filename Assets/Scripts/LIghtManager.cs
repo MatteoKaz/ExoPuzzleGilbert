ddd666331539c32dac9957846e0a36966ef18d5e
BenMartFilio <ecobmartinezfilio@gmail.com>
2025-11-06 00:36:30 +0100

    [BMF] Light Turn

---
+++ Assets/Scripts/LIghtManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using UnityEngine;
+
+public class LIghtManager : MonoBehaviour
+{
+    [SerializeField] public GameObject lighting;
+    [SerializeField] public float transitionDuree;
+    [SerializeField] public float valueLight;
+    private Light lum;
+    public bool state = false;
+
+    private void Start()
+    {
+        TurnOn();
+        state = lighting.active;
+        Debug.Log(state);
+    }
+
+    public void TurnOn()
+    {
+        if (lighting != null)
+        {
+            lum = lighting.GetComponent<Light>();
+            StartCoroutine(IncreaseOpacity());
+            state = true;
+            lighting.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("LightIsNull");
+        }
+    }
+
+    public void TurnOff()
+    {
+        if (lighting != null)
+        {
+            StartCoroutine(DecreaseOpacity());
+
+            state = false;
+            lighting.SetActive(false);
+        }
+    }
+
+
+    private IEnumerator IncreaseOpacity()
+    {
+        float temps = 0f;
+
+        while (temps < transitionDuree)
+        {
+            temps += Time.deltaTime;
+            lum.intensity = Mathf.Lerp(0f, valueLight, temps / transitionDuree);
+            yield return null;
+        }
+
+    }
+    private IEnumerator DecreaseOpacity()
+    {
+        float temps = 0f;
+
+        while (temps < transitionDuree)
+        {
+            temps += Time.deltaTime;
+            lum.intensity = Mathf.Lerp(valueLight, 0f, temps / transitionDuree);
+            yield return null;
+        }
+    }
+}
