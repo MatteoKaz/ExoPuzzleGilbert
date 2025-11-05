39d82e7d6d338124a38bfccb2126a8360a3e98f5
MatteoKaz <matteokazan@gmail.com>
2025-11-05 14:48:45 +0100

    MK Animation

--- Assets/Character/PlayerMovement.cs
+++ Assets/Character/PlayerMovement.cs
@@ -5,13 +5,14 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody rb;
     public float speed = 0f;
     private Vector3 Direction;
-
+    private Animator _animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
          speed = 0f;
-
+        _animator = GetComponent<Animator>();
+        _animator.SetFloat("Speed", -speed);
     }
 
     // Update is called once per frame
@@ -19,7 +20,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (rb != null)
         {
-            
+            _animator.SetFloat("Speed", -speed);
                 rb.linearVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y, speed);
             
            
