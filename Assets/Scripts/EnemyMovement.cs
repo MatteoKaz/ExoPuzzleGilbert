75d2e08265eb206b920649ec6e08f6c4a69198fe
MaelCompin <mael.compin@gmail.com>
2025-11-05 14:49:06 +0100

    ILI_ScriptEnemy

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [Header("Detection Settings")]
+    [SerializeField] private Transform player;
+    [SerializeField] private float detectionRange = 5f;
+    [SerializeField] private float runSpeed = 3f;
+
+    [Header("Animation")]
+    private Animator animator;
+    
+    private bool isRunning = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        
+        if (player == null)
+        {
+            player = GameObject.Find("Idle_0")?.transform;
+        }
+    }
+
+    void Update()
+    {
+        if (player == null) return;
+
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distanceToPlayer <= detectionRange)
+        {
+            RunAway();
+        }
+        else
+        {
+            StopRunning();
+        }
+    }
+
+    void RunAway()
+    {
+        if (!isRunning)
+        {
+            isRunning = true;
+            animator.Play("Enemy_RunAttack");
+        }
+
+        Vector3 directionAwayFromPlayer = (transform.position - player.position).normalized;
+        transform.Translate(directionAwayFromPlayer * runSpeed * Time.deltaTime, Space.World);
+        
+        if (directionAwayFromPlayer.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+
+    void StopRunning()
+    {
+        if (isRunning)
+        {
+            isRunning = false;
+            animator.Play("Enemy_Idle");
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, detectionRange);
+    }
+}
