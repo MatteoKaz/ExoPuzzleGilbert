33cf754fd81f51d83239b9655cbefe7222014884
BenMartFilio <ecobmartinezfilio@gmail.com>
2025-11-27 18:16:50 +0100

    [BMF] Fin code traversable

--- Assets/Scripts/ProjectDecalWall.cs
+++ Assets/Scripts/ProjectDecalWall.cs
@@ -5,10 +5,7 @@ public class ProjectDecalWall : MonoBehaviour
     [SerializeField] public GameObject ToFollow;
     void Update()
     {
-        if (ToFollow != null)
-        {
-            Vector3 positionToFollow = ToFollow.transform.position;
-            Vector3 newPosition = new Vector3(18.93f,positionToFollow.y,positionToFollow.z);
-        }
+            Vector3 newPosition = new Vector3(18.93f,transform.position.y,transform.position.z);
+            transform.position = newPosition;
     }
 }
