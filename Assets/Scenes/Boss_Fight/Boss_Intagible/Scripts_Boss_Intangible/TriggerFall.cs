28c71db4110541acaa9fea762a8fed5c9e49d1ef
Ilana LINARES <ilana.linares@brassart.Fr>
2025-11-28 10:11:03 +0100

    [ILI]Boss_FightTestBoxColliderCordeIntangibilté

---
+++ Assets/Scenes/Boss_Fight/Boss_Intagible/Scripts_Boss_Intangible/TriggerFall.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class TriggerFall : MonoBehaviour
+{
+    public Rigidbody2D cubeRigidBody;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            cubeRigidBody.gravityScale = 1; // Active la gravité → le cube tombe
+        }
+    }
+}
