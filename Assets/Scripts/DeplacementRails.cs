c569bbf1fccb3df7c33787404c28a78a48e2ffd6
BenMartFilio <ecobmartinezfilio@gmail.com>
2025-12-13 23:39:53 +0100

    [BMF] correction d'une partie de l'aimant

--- Assets/Scripts/DeplacementRails.cs
+++ Assets/Scripts/DeplacementRails.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using TreeEditor;
 using UnityEngine;
 using UnityEngine.XR.Content.Interaction;
 
@@ -7,32 +8,54 @@ public class DeplacementRails : MonoBehaviour
     public float minClamp = -1f;
     public float maxClamp = -0.15f;
     List<Collision> listColl = new List<Collision>();
+    public float forcePoussee = 1;
+    Rigidbody rb;
+    Transform rail;
+
+    private void Start()
+    { 
+        rb = GetComponent<Rigidbody>();
+        rail = transform.parent;
+    }
     void FixedUpdate()
     {
         //Vector3 pos = transform.position;
         Vector3 pos = transform.localPosition;
         pos.z = Mathf.Clamp(pos.z, minClamp, maxClamp);
-        if (pos.z >= maxClamp || pos.z <= minClamp)
+        Vector3 rPos = transform.parent.TransformPoint(pos);
+        if (transform.localPosition.z >= maxClamp || transform.localPosition.z <= minClamp)
         {
+            rb.MovePosition(rPos);
             if (listColl.Count > 0)
             {
-                GetComponent<Rigidbody>().isKinematic = true;
+                rb.constraints = RigidbodyConstraints.FreezeAll;
+            //    rb.isKinematic = true;
+                
             }
         }
-        transform.localPosition = pos;
+        
     }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.gameObject.GetComponent<PousseurAimant>() != null)
         {
             listColl.Add(collision);
-            gameObject.layer = LayerMask.NameToLayer("ShadowPlayerTraversable");
+          //  gameObject.layer = LayerMask.NameToLayer("ShadowPlayerTraversable");
         }
         else
         {
-            if (listColl.Count <= 0)
+            if (listColl.Count > 0)
             {
-                GetComponent<Rigidbody>().isKinematic = true;
+             //   GetComponent<Rigidbody>().isKinematic = true;
+            }
+            else if (listColl.Count <= 0)
+            {
+             //   rb.isKinematic = false;
+                rb.constraints = RigidbodyConstraints.FreezePositionX |
+                 RigidbodyConstraints.FreezePositionY |
+                 RigidbodyConstraints.FreezeRotation;
+
+                gameObject.layer = LayerMask.NameToLayer("Aimant");
             }
         }
        
@@ -45,7 +68,11 @@ public class DeplacementRails : MonoBehaviour
             listColl.Remove(collision);
             if (listColl.Count <= 0)
             {
-                GetComponent<Rigidbody>().isKinematic = false;
+             //   rb.isKinematic = false;
+                rb.constraints = RigidbodyConstraints.FreezePositionX |
+                 RigidbodyConstraints.FreezePositionY |
+                 RigidbodyConstraints.FreezeRotation;
+
                 gameObject.layer = LayerMask.NameToLayer("Aimant");
             }
         }
@@ -53,11 +80,33 @@ public class DeplacementRails : MonoBehaviour
         {
             if (listColl.Count <= 0)
             {
-                GetComponent<Rigidbody>().isKinematic = false;
+             //   rb.isKinematic = false;
+                rb.constraints = RigidbodyConstraints.FreezePositionX |
+                 RigidbodyConstraints.FreezePositionY |
+                 RigidbodyConstraints.FreezeRotation;
+
                 gameObject.layer = LayerMask.NameToLayer("Aimant");
             }
         }
        
     }
-    
+
+    private void OnCollisionStay(Collision collision)
+    {
+        PousseurAimant pousseur = collision.gameObject.GetComponent<PousseurAimant>();
+        if (pousseur == null)
+        {
+            return;
+        }
+        Debug.Log("POUSSE");
+
+        Vector3 toPusher = rb.position - collision.transform.position;
+        float sign = Mathf.Sign(Vector3.Dot(toPusher, rail.forward));
+        Vector3 pushDir = rail.forward * sign;
+        //    Vector3 pousseeDirection = new Vector3(0, 0, collision.contacts[0].normal.z); //prend la direction par laquelle le joueur pousse le cube (il prend la normale au point de contact des deux plans)
+        // rb.AddForce(Vector3.forward * forcePoussee, ForceMode.Force);
+
+        rb.AddForce(pushDir * forcePoussee, ForceMode.Force);
+    }
+
 }
