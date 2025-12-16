73ded6db63f38cf1cf621e397f21119c22ed7cf8
BenMartFilio <ecobmartinezfilio@gmail.com>
2025-12-16 13:27:09 +0100

    [BMF] Son Traversable + applaudissement

--- Assets/Scripts/TraversableMur.cs
+++ Assets/Scripts/TraversableMur.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class TraversableMur : MonoBehaviour
 {
+    private AudioSource audioMur;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        audioMur = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void PlaySoundMur(float volumeSound)
+    {
+        if (audioMur != null)
+        {
+            audioMur.volume = volumeSound;
+            audioMur.Play();
+        }
     }
 }
